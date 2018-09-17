dc6c2be3a78348ce4d31a7acf96257fceef5ff0f
e <wufangbing@szunicom.com>
2018-09-17 17:51:14 +0800

    update

--- pages/navigator/navigator.js
+++ pages/navigator/navigator.js
@@ -1,1 +1,41 @@
-Page({})
+Page({
+  data:{
+    latitude: 22.540822,
+    longitude: 113.934457,
+    scale: 16,
+    markers: [{
+      latitude: 22.540822,
+      longitude: 113.934457,
+      name: '腾讯大厦',
+      callout: {
+        content: '气泡气泡气泡',
+        color: '#fff',
+        fontSize: 15,
+        borderRadius: 10,
+        bgColor: '#000',
+        display: 'ALWAYS'
+      },
+      label: {
+        content: '这是腾讯大厦',
+        color: '#333',
+        x: 4, 
+        y: 0,
+        borderWidth: 1,
+        borderColor: '#000',
+        borderRaddius: 5,
+        bgColor: '#fff',
+        padding: 2,
+        textAlign: 'center'
+      }
+    }]
+  },
+  loadImage(e) {
+    console.log(e)
+  },
+  regionchange(e) {
+    // console.log(e)
+  }, 
+  clickmap(e) {
+    console.log(e)
+  },
+})
